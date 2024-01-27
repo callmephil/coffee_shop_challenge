39e280f461ed4ee2d783fdae70bbc1fa1c147f28
Phil <phil@polarstork.com>
2024-01-27 18:33:11 +0200

    Added PathUrlStrategy for routing

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,31 @@
 import 'package:coffee_shop_challenge/constants/constants.dart';
 import 'package:coffee_shop_challenge/routes/app_routes.dart';
 import 'package:flutter/material.dart';
+import 'package:url_strategy/url_strategy.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  /// removes the # from the url
+  setPathUrlStrategy();
+
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+
+    AppAssets.preCacheAssets(context);
+  }
 
   @override
   Widget build(BuildContext context) {
