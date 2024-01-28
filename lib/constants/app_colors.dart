9de6b3baa8dc4b12e1b63c783a487a80f100269f
Phil <phil@polarstork.com>
2024-01-28 13:07:42 +0200

    Added NavyBlue color

--- lib/constants/app_colors.dart
+++ lib/constants/app_colors.dart
@@ -8,6 +8,7 @@ class AppColors {
   static const Color orange = Color(0xFFC67C4E);
   static const Color orangeLight = Color(0xFFFFF5EE);
   static const Color yellowDark = Color(0xFFFBBE21);
+  static const Color navyBlue = Color(0xFF2F4B4E);
 
   static const Color black = Color(0xFF2F2D2C);
   static const Color lightGrey = Color(0xFF9B9B9B);
