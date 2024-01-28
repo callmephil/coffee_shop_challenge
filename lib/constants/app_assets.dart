4701b848db34b88f65e191b422840cf3aa33be7a
Phil <phil@polarstork.com>
2024-01-28 13:07:19 +0200

    Fixed assets namings

--- lib/constants/app_assets.dart
+++ lib/constants/app_assets.dart
@@ -5,17 +5,17 @@ import 'package:flutter/material.dart'
 class AppAssets {
   const AppAssets._();
 
-  static const String appIcon = 'assets/avatars';
-  static const String iconsPath = 'assets/icons';
-  static const String imagePath = 'assets/images';
+  // static const String _appIcon = 'assets/avatars';
+  // static const String _iconsPath = 'assets/icons';
+  static const String _imagePath = 'assets/images';
   static const String gifsPath = 'assets/gifs';
 
-  static const String latte = '$imagePath/latte.png';
-  static const String americano = '$imagePath/americano.png';
-  static const String cappuccino = '$imagePath/cappuccino.png';
-  static const String cappuccinoLarge = '$imagePath/cappuccino_large.png';
-  static const String latteSpecial = '$imagePath/latte_special.png';
-  static const String promotion1 = '$imagePath/promotion_1.png';
+  static const String latte = '$_imagePath/latte.png';
+  static const String americano = '$_imagePath/americano.png';
+  static const String cappuccino = '$_imagePath/cappuccino.png';
+  static const String cappuccinoLarge = '$_imagePath/cappuccino_large.png';
+  static const String latteSpecial = '$_imagePath/latte_special.png';
+  static const String promotion1 = '$_imagePath/promotion_1.png';
 
   static const String splash = '$gifsPath/running_mug.gif';
 
