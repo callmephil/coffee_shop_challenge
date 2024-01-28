4174324d92caaf178fd4b67d1aba3293f51ab543
Phil <phil@polarstork.com>
2024-01-28 13:08:12 +0200

    Added app route enums

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -3,7 +3,23 @@ import 'package:coffee_shop_challenge/screens/details_screen.dart';
 import 'package:coffee_shop_challenge/screens/favorites_screen.dart';
 import 'package:coffee_shop_challenge/screens/home_screen.dart';
 import 'package:coffee_shop_challenge/screens/profile_screen.dart';
+import 'package:coffee_shop_challenge/screens/splash_screen.dart';
 import 'package:go_router/go_router.dart';
+
+enum AppRoutesEnum {
+  splash('/'),
+  home('/home'),
+  details('/details/:id'),
+  order('/order/:id'),
+  delivery('/delivery'),
+  favorites('/favorites'),
+  profile('/profile'),
+  ;
+
+  const AppRoutesEnum(this.path);
+
+  final String path;
+}
 
 class AppRoutes {
   const AppRoutes._();
@@ -13,7 +29,13 @@ class AppRoutes {
       initialLocation: '/',
       routes: [
         GoRoute(
-          path: '/',
+          path: AppRoutesEnum.splash.path,
+          name: AppRoutesEnum.splash.name,
+          builder: (context, state) => const SplashScreen(),
+        ),
+        GoRoute(
+          path: AppRoutesEnum.home.path,
+          name: AppRoutesEnum.home.name,
           builder: (context, state) => const HomeScreen(),
         ),
         GoRoute(
